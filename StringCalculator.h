e20f72a937c489f819759cc00bd5ff25c590da0f
PraneethaAragonda <praneethaaragonda2@gmail.com>
2024-09-14 13:15:44 +0530

    Update StringCalculator.h Resolving the run test errors

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,7 +1,10 @@
+#include <string.h>
+
 // Function Declaration
 int add(const char* input);
 
 // Implementation of the add function
 int add(const char* input) {
-   
+   if (input == NULL || strlen(input) == 0) {
+        return 0;
     }
